7872c41c5e3e96b98c5ba4a63543c012bf13a73d
Hagwan Mohammed <79087273+Hagwan@users.noreply.github.com>
2024-10-15 16:56:12 +0300

    Refactor routes and onboarding screen

--- lib/constants/routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const splash = '/';
-  static const authWrapper = '/auth-wrapper';
-  static const login = '/login';
-  static const signup = '/signup';
-  static const home = '/home';
-  static const onboarding = '/onboarding';  // New onboarding route
-}
--- lib/views/screens/splash/onboarding_screen.dart
+++ lib/views/screens/splash/onboarding_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
-import '../constants/routes.dart';
+import '../../constants/routes.dart';
 
 class OnboardingScreen extends StatelessWidget {
   @override
